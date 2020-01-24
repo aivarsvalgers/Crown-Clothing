c6fa2a78868f01564bf259b12449faefb9b2fd84
Aivvory <41149583+Aivvory@users.noreply.github.com>
2020-01-24 14:55:41 +0200

    Updating the app to be PWA compliant

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+
 import { store, persistor } from './redux/store';
+import * as serviceWorker from './serviceWorker';
 
 import './index.css';
 import App from './App';
@@ -19,3 +21,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
