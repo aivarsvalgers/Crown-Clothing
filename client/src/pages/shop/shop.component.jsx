9f6a2000c347a3c84a9046584f88379507aa7b07
Aivvory <41149583+Aivvory@users.noreply.github.com>
2020-01-24 10:57:29 +0200

    Adding Performance optimizations

--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -1,28 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
-import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.component';
-import CollectionPageContainer from '../collection/collection.container';
+import Spinner from '../../components/spinner/spinner.component';
 
 const ShopPage = ({ fetchCollectionsStart, match }) => {
   useEffect(() =>{
     fetchCollectionsStart();
   }, [fetchCollectionsStart]);
 
+  const CollectionsOverviewContainer = lazy(() => import('../../components/collections-overview/collections-overview.component'));
+  const CollectionPageContainer = lazy(() => import('../collection/collection.container'));
+
   return (
       <div className='shop-page'>
-        <Route
-          exact
-          path={`${match.path}`}
-          component={CollectionsOverviewContainer}
-        />
-        <Route
-          path={`${match.path}/:collectionId`}
-          component={CollectionPageContainer}
-        />
+      <Suspense fallback={<Spinner />}>
+          <Route
+            exact
+            path={`${match.path}`}
+            component={CollectionsOverviewContainer}
+          />
+          <Route
+            path={`${match.path}/:collectionId`}
+            component={CollectionPageContainer}
+          />
+        </Suspense>
       </div>
   );
 };
